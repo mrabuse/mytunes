04516d587d65db85c3314778ce342b470102abec
mrabuse <cogglemat@gmail.com>
2017-02-15 17:17:48 -0600

    Pass SongQueue specs

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,9 +1,31 @@
 // SongQueue.js - Defines a backbone model class for the song queue.
 var SongQueue = Backbone.Collection.extend({
-
+  
   model: SongModel,
 
   initialize: function() {
-  }
+    var context = this;
 
+    this.on('ended', function () {
+      context.shift();
+
+      if (context.length > 0) {
+        context.playFirst();
+      }
+    });
+
+    this.on('add', function () {
+      if (context.length === 1) {
+        context.playFirst();
+      }
+    });
+
+    this.on('dequeue', function () {
+      context.remove();
+    });
+  },
+
+  playFirst: function() {
+    this.at(0).play();
+  },
 });
